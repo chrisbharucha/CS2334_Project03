58e630e6f9e3ddcd24c4afb292c5f55f7aab2253
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-25 09:34:48 -0500

    fixed dateTimeDifferentZone method

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -15,13 +15,14 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 	private static final int ARRAY_SIZE = 10;
 	private static final int MILLI_CONSTANT = 1000;
 	Calendar calendar; //calendar used for data
-	Date today;
+	Date today; //date used for different TimeZones
 	HashMap<String, String> map;
 	
 	
 	public DateTimeOne() {
 		//initializing the calendar and HashMap
 		calendar = new GregorianCalendar();
+		today = new Date();
 		map = new HashMap<String, String>();
 	}
 	
@@ -56,7 +57,6 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 	public void dateTimeOfOtherCity() {
 		//format for the hour output is as follows:
 		SimpleDateFormat hourFormat = new SimpleDateFormat("k:mm");
-		today = new Date();
 		
 		//printing out default TimeZone
 		System.out.println("Time on Server: " + hourFormat.format(today));
@@ -78,27 +78,23 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 		//format for the date output of other timeZone is as follows:
 		SimpleDateFormat zoneDateFormat = new SimpleDateFormat("MM/dd/yyyy k:mm");
 		
-		//creating calendars for the time zones
-		Calendar gmt = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-		Calendar bst = Calendar.getInstance(TimeZone.getTimeZone("GMT+6"));
-		Calendar cst = Calendar.getInstance(TimeZone.getTimeZone("GMT-6"));
-		
-		//Date today = new Date();
-				
-		//Getting the String representations of the date to store in HashMap
-		String gmtValue = zoneDateFormat.format(gmt.getTime());
-		String bstValue = zoneDateFormat.format(bst.getTime());
-		String cstValue = zoneDateFormat.format(cst.getTime());
-		
-		//adding the time zones to the HashMap
+		//changing to GMT TimeZone, adding to HashMap and printing
+		zoneDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		String gmtValue = zoneDateFormat.format(today);
 		map.put("GMT", gmtValue);
+		System.out.println("GMT: " + zoneDateFormat.format(today));
+				
+		//changing to BST TimeZone, adding to HashMap and printing
+		zoneDateFormat.setTimeZone(TimeZone.getTimeZone("BST"));
+		String bstValue = zoneDateFormat.format(today);
 		map.put("BST", bstValue);
+		System.out.println("BST (90E): " + zoneDateFormat.format(today));
+				
+		//changing to CST TimeZone, adding to HashMap and printing
+		zoneDateFormat.setTimeZone(TimeZone.getTimeZone("CST"));
+		String cstValue = zoneDateFormat.format(today);
 		map.put("CST", cstValue);
-		
-		//printing out in the right format
-		System.out.println("GMT: " + gmtValue);
-		System.out.println("BST: " + bstValue);
-		System.out.println("CST: " + cstValue);
+		System.out.println("CST (90W): " + zoneDateFormat.format(today));
 	}
 	
 	public void timeZoneHashMap() {
