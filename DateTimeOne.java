55f08d03d39264f7563774c8634bd7c936bc1330
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-24 20:17:15 -0500

    finished valueOfSecond and sleepForFiveSec method

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,7 +1,8 @@
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-	
+
+	private static final int MILLI_CONSTANT = 1000;
 
 	
 	public DateTimeOne() {
@@ -9,12 +10,16 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 	}
 	
 	public int getValueOfSecond() {
-	return 0;
+	return (int)(System.currentTimeMillis() / MILLI_CONSTANT);
 	}
 	
-
 	public void sleepForFiveSec() {
-		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.out.println("me no wanna go sleep");
+		}
 	}
 	
 	public void dateTimeNow() {
