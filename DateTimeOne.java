390d1c3024a5a85d3badaad2f746db47b2ee4b1c
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-24 21:23:34 -0500

    finished dateTimeOfOtherCity method

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,6 +1,8 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
@@ -27,14 +29,25 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 	}
 	
 	public void dateTimeNow() {
-		//format for the output is as follows:
-		SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy hh:mm a");
+		//format for the date output is as follows:
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy h:mm a");
 		System.out.println("Current Date/Time: " + dateFormat.format(calendar.getTime()));
 	}
 	
-	
 	public void dateTimeOfOtherCity() {
+		//format for the hour output is as follows:
+		SimpleDateFormat hourFormat = new SimpleDateFormat("k:mm");
 		
+		//getting the time zones for the GMT, BST, and CST time zones
+		TimeZone gmt = TimeZone.getTimeZone("GMT");
+		TimeZone bst = TimeZone.getTimeZone("BST");
+		TimeZone cst = TimeZone.getTimeZone("CST");
+		
+		//printing out in the right format
+		System.out.println("Time on Server: " + hourFormat.format(calendar.getTime()));
+		System.out.println("GMT: " + hourFormat.format(gmt));
+		System.out.println("BST (90E): " + hourFormat.format(bst));
+		System.out.println("CST (90W): " + hourFormat.format(cst));
 	}
 	
 	public void dateTimeDifferentZone() {
