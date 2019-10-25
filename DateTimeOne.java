6e0e7eb15ac6882942bc61e43682b0410a8ec09a
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-24 20:06:31 -0500

    framework for section 1

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,5 +1,36 @@
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-   
+	
+
+	
+	public DateTimeOne() {
+		
+	}
+	
+	public int getValueOfSecond() {
+	return 0;
+	}
+	
+
+	public void sleepForFiveSec() {
+		
+	}
+	
+	public void dateTimeNow() {
+		
+	}
+	
+	
+	public void dateTimeOfOtherCity() {
+		
+	}
+	
+	public void dateTimeDifferentZone() {
+		
+	}
+	
+	public void timeZoneHashMap() {
+		
+	}
 }
