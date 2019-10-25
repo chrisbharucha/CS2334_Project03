7dc13214e5c2bebfa215f665e7fb43971f6543f0
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-25 13:16:44 -0500

    created DateTimeTwo.java and laid out the framework for the class

---
+++ DateTimeTwo.java
@@ -0,0 +1,30 @@
+
+public class DateTimeTwo {
+	
+	private String tenthDay;
+	private String eighteenthDay;
+	
+	public DateTimeTwo() {
+		
+	}
+	
+	public void daysOfCurrentMonth() {
+		System.out.println("The tenth day of this month is " + tenthDay + " and eighteenth is " + eighteenthDay);
+	}
+	
+	public void daysOfAnyMonth(int month, int year) {
+		
+	}
+	
+	public void compareYear() {
+		
+	}
+	
+	public void dateHashMap() {
+		
+	}
+	
+	public void dateHashMapSorted() {
+		
+	}
+}
