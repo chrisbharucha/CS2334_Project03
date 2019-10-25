acc39c713c8e77f00b2791890af137631d040631
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-25 16:39:28 -0500

    finished daysOfCurrentMonth()

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -1,19 +1,34 @@
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.Calendar;
+
 public class DateTimeTwo {
 	
-	private String tenthDay;
-	private String eighteenthDay;
+	private Calendar calendar; //calendar used for data
 	
 	public DateTimeTwo() {
-		
+		//initializing the local private variables
+		calendar = Calendar.getInstance();
 	}
 	
 	public void daysOfCurrentMonth() {
-		System.out.println("The tenth day of this month is " + tenthDay + " and eighteenth is " + eighteenthDay);
+		//Creating a YearMonth to find the value of tenth and eighteenth day as an int
+		YearMonth ym = YearMonth.now();
+		LocalDate tenthDay = ym.atDay(10);
+		LocalDate eighteenthDay = ym.atDay(18);
+		
+		//calling getDayOfWeek() to convert int value to a String
+		System.out.println("The tenth day of this month is " + tenthDay.getDayOfWeek() + " and eighteenth is " + eighteenthDay.getDayOfWeek());
 	}
 	
 	public void daysOfAnyMonth(int month, int year) {
 		
+		
+		
+		System.out.println("For the year (" + year + ") and month (" + month + "), the fifteenth day"
+				+ " is " + fifteenthDay + " and the last day is " + lastDay;
 	}
 	
 	public void compareYear() {
